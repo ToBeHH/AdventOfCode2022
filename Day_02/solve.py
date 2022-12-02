9f8802081212643e8a36acfce880df42f381d0c0
Tobias Schulz-Hess <tobias.schulz-hess@iu.org>
2022-12-02 10:47:03 +0100

    Changes for day 2 riddle for second part

--- Day_02/solve.py
+++ Day_02/solve.py
@@ -11,31 +11,31 @@ for line in lines:
     # since there are only 9 possibile combinations, I write them out
     # need to add strio() to remove trailing new-line
     if line.strip() == "A X":
-        # Rock vs. Rock - Draw, rock is 1 
-        score = score + 1 + 3
-    if line.strip() == "A Y":
-        # Rock vs. Paper - I win, paper is 2
-        score = score + 2 + 6
-    if line.strip() == "A Z":
-        # Rock vs. Scissors - I loose, scissor is 3
+        # Rock and I need to loose, so I need to choose Scissors - I loose, scissor is 3
         score = score + 3 + 0
+    if line.strip() == "A Y":
+        # Rock and I need to draw, so I need to choose rock - Draw, rock is 1 
+        score = score + 1 + 3
+    if line.strip() == "A Z":
+        # Rock and I need to win, so I need to choose Paper - I win, paper is 2
+        score = score + 2 + 6
     if line.strip() == "B X":
-        # Paper vs. Rock - I loose, rock is 1
+        # Paper and I need to loose, so I need to choose Rock - I loose, rock is 1
         score = score + 1  + 0
     if line.strip() == "B Y":
-        # Paper vs. Paper - Draw, paper is 2
+        # Paper and I need to draw, so I need to choose Paper - Draw, paper is 2
         score = score + 2 + 3
     if line.strip() == "B Z":
-        # Paper vs. Scissors - I win, scissor is 3
+        # Paper and I need to win, so I need to choose Scissors - I win, scissor is 3
         score = score + 3 + 6
     if line.strip() == "C X":
-        # Scissors vs. Rock - I win, rock is 1
-        score = score + 1 + 6
-    if line.strip() == "C Y":
-        # Scissors vs. Paper - I loos, paper is 2
+        # Scissors and I need to loose, so I need to choose Paper - I loos, paper is 2
         score = score + 2 + 0
-    if line.strip() == "C Z":
-        # Scissors vs. Scissors - Draw - scissor is 3
+    if line.strip() == "C Y":
+        # Scissors and I need to draw, so I need to choose Scissors - Draw - scissor is 3
         score = score + 3 + 3
+    if line.strip() == "C Z":
+        # Scissors and I need to win, so I need to choose Rock - I win, rock is 1
+        score = score + 1 + 6
 
 print("The total score is %d" % score)
