5e4d8585856998b44477e8984a8333748cfa8283
Tobias Schulz-Hess <github.com@schulz-hess.de>
2022-12-03 11:27:08 +0100

    Added part 2 of the riddle wih new sample data

--- Day_03/solve.py
+++ Day_03/solve.py
@@ -5,28 +5,31 @@ file = open('input.txt', 'r')
 lines = file.readlines()
 
 sum_of_priorities = 0
+rucksack = ['','','']
+rucksack_index = 0
 
 # read the lines of the file
 for line in lines:
     # remove new-line character
     line = line.strip()
-    # find center
-    half = int(len(line)/2)
-    # build two compartments
-    first_compartment = line[:half]
-    second_compartment = line[half:]
+    # put them into one rucksack
+    rucksack[rucksack_index] = line
+    rucksack_index = rucksack_index + 1
+    # are all rucksacks filled?
+    if rucksack_index == 3:
+        rucksack_index = 0
     
-    # now find common characters in each string
-    common = set(first_compartment).intersection(second_compartment)
-    for common_character in common:
-        # get the priority for each character
-        if common_character.islower():
-            # we use the ascii value to determine the priority
-            priority = ord(common_character) - ord('a') + 1
-        else:
-            # we use the ascii value to determine the priority
-            priority = ord(common_character) - ord('A') + 27
-        # sum them up
-        sum_of_priorities = sum_of_priorities + priority
+        # now find common characters in each string
+        common = set(rucksack[0]).intersection(rucksack[1]).intersection(rucksack[2])
+        for common_character in common:
+            # get the priority for each character
+            if common_character.islower():
+                # we use the ascii value to determine the priority
+                priority = ord(common_character) - ord('a') + 1
+            else:
+                # we use the ascii value to determine the priority
+                priority = ord(common_character) - ord('A') + 27
+            # sum them up
+            sum_of_priorities = sum_of_priorities + priority
 
 print("The sum of the priorities is %d" % sum_of_priorities)
